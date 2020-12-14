90617e48fd6c2dee60259ff70d3cf99c6fb33a89
Muhammed Ali Zengin <alizengin99@gmail.com>
2020-12-14 10:35:30 +0100

    Create Main.java

---
+++ OOP Questions/Carpet/Main.java
@@ -0,0 +1,8 @@
+package Inheritance.Repl.Carpet;
+
+public class Main {
+    public static void main(String[] args) {
+        Carpet c = new Carpet(20.0, 20.0 , 2.0 ,true);
+        System.out.println(c.totalPrice);
+    }
+}
