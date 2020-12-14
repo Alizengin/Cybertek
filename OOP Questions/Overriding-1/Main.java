5bc8752cc7499dfc73749a4287d4436e3d69bed1
Muhammed Ali Zengin <alizengin99@gmail.com>
2020-12-14 11:09:46 +0100

    Create Main.java

---
+++ OOP Questions/Overriding-1/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.Repl.Education;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LawSchool l = new LawSchool(3);
+        System.out.println(l.graduationRequirements());
+
+    }
+}
